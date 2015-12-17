42b6b60e57f0d1e4bfc828f52354b82fe84de187
Owen Tran <owen_327@hotmail.com>
2015-12-17 12:45:21 +0000

    Add test for flash (solves exercise 7.3)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,5 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty? #or
+    assert_not_empty flash
   end
 end
